4e75e58b55723f6a35ebe0a1084b281f8184fa2a
Qanpi <aleksei.terin@outlook.com>
2022-05-09 00:28:48 +0300

    passable tab size support

--- src/com/qanpi/Editor.java
+++ src/com/qanpi/Editor.java
@@ -2,8 +2,10 @@ package com.qanpi;
 
 import javax.swing.*;
 import javax.swing.text.*;
+import java.awt.*;
 import java.io.File;
 import java.io.IOException;
+import java.util.Enumeration;
 
 public class Editor {
     private File currentFile;
@@ -29,6 +31,21 @@ public class Editor {
         currentFile = f;
     }
 
+//    void setTabSpacing() {
+//        TabStop[] tabs = new TabStop[1];
+//        tabs[0] = new TabStop(150, TabStop.ALIGN_LEFT, TabStop.LEAD_NONE);
+//        TabSet tabset = new TabSet(tabs);
+//
+//        SimpleAttributeSet sas = new SimpleAttributeSet();
+//        StyleConstants.setTabSet(sas, tabset);
+//        StyleConstants.setForeground(sas, Color.red);
+//
+//        component.setParagraphAttributes(sas, true);
+//        component.setCharacterAttributes(sas, false);
+//
+//        System.out.println(component.getParagraphAttributes().getAttributeNames().nextElement());
+//    }
+
     String getText() {
         return component.getText();
     }
@@ -41,6 +58,8 @@ public class Editor {
 }
 
 class EditorFilter extends DocumentFilter {
+    final int TAB_SIZE = 4;
+
     @Override
     public void insertString(FilterBypass fb, int offset, String string, AttributeSet attr) throws BadLocationException {
         super.insertString(fb, offset, string.replace("\t", "    "), attr);
@@ -49,14 +68,33 @@ class EditorFilter extends DocumentFilter {
     @Override
     public void replace(FilterBypass fb, int offset, int length, String text, AttributeSet attrs) throws BadLocationException {
         if (text == null) text = ""; //to prevent errors with .setText(null) as pointed out by SOF
+
+        if(text.equals("\t")) {
+            String pretext = fb.getDocument().getText(0, offset);
+            int lineStart = pretext.lastIndexOf('\n') + 1;
+
+            int filling = TAB_SIZE - (offset - lineStart) % TAB_SIZE; //find the number of characters until the tab column
+            super.replace(fb, offset, length, text.replace("\t", " ".repeat(filling)), attrs);
+            return;
+        }
         super.replace(fb, offset, length, text.replace("\t", "    "), attrs);
     }
 
     @Override
     public void remove(FilterBypass fb, int offset, int length) throws BadLocationException {
-        //TODO: deal with the weirdness
-        if (length == 1 && fb.getDocument().getText(offset - 4, 4).equals("    "))
-            super.remove(fb, offset - 4, 4);
+
+        if (length == 1) { //assuming the backspace is pressed, but also triggered by delete key :(
+            String pretext = fb.getDocument().getText(0, offset);
+            int lineStart = pretext.lastIndexOf('\n') + 1;
+            int rem = (offset - lineStart) % TAB_SIZE; //find the number of characters over the tab column
+
+            String text = fb.getDocument().getText(offset - rem, rem + 1);
+            if (text.isBlank()) {
+                super.remove(fb, offset - rem, rem + 1);
+                return;
+            }
+        }
+
         super.remove(fb, offset, length);
     }
 }
